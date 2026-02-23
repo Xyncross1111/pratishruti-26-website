cdb86acef388d706717baf0329393b56ac09e715
soumyyaagrawal <soumyaagrawal2k6@gmail.com>
2026-02-23 17:28:20 +0530

    updated credits page

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import React from "react"
 import type { Metadata } from 'next'
 import { Geist, Geist_Mono } from 'next/font/google'
 import { Analytics } from '@vercel/analytics/next'
-import { SpeedInsights } from '@vercel/speed-insights/next'
+// import { SpeedInsights } from '@vercel/speed-insights/next'
 import { PuzzleProvider } from '@/components/atlantis/PuzzleProvider'
 import PuzzleOverlay from '@/components/atlantis/PuzzleOverlay'
 import './globals.css'
@@ -46,7 +46,7 @@ export default function RootLayout({
           <PuzzleOverlay />
         </PuzzleProvider>
         <Analytics />
-        <SpeedInsights />
+        {/* <SpeedInsights /> */}
       </body>
     </html>
   )
